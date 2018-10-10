e7c8ebed071fcd38d5e8f8598441873fb704b4d3
이우상 <lus0506@gmail.com>
2018-10-10 19:08:04 +0900

    조금수정

--- 3D게임2 과제01(이우상-2013180028)/3DGame_Texture/3DGame_Texture/Mesh.cpp
+++ 3D게임2 과제01(이우상-2013180028)/3DGame_Texture/3DGame_Texture/Mesh.cpp
@@ -417,7 +417,7 @@ CHeightMapGridMesh::CHeightMapGridMesh(ID3D12Device *pd3dDevice, ID3D12GraphicsC
 			pVertices[i].m_xmf4Diffuse = Vector4::Add(OnGetColor(x, z, pContext), xmf4Color);
 			pVertices[i].m_xmf2TexCoord0 = XMFLOAT2(float(x) / float(cxHeightMap - 1), float(czHeightMap - 1 - z) / float(czHeightMap - 1));
 
-			if (fHeight < 235.f)
+			if (fHeight < 240.f)
 				pVertices[i].m_xmf2TexCoord1 = XMFLOAT2(float(x) / float(m_xmf3Scale.x*0.5f), float(z) / float(m_xmf3Scale.z*0.5f));
 			else			
 				pVertices[i].m_xmf2TexCoord2 = XMFLOAT2(float(x) / float(m_xmf3Scale.x*0.5f), float(z) / float(m_xmf3Scale.z*0.5f));
